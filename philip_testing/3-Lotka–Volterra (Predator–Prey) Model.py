6b0b2ac8c6a74de8dbaf80996a44403a9729e388
thegooda <philipnguyen5050@gmail.com>
2025-10-21 11:28:52 -0700

    update

--- philip_testing/3-Lotka–Volterra (Predator–Prey) Model.py
+++ philip_testing/3-Lotka–Volterra (Predator–Prey) Model.py
@@ -7,9 +7,31 @@ from scipy.integrate import odeint
 # y˙​=δxy−γy
 
 #Task:
+#Recover the nonlinear interaction terms xy
 
-#Simulate with odeint.
-#Fit with PySINDy using polynomial features (degree 2).
-#Interpret which terms it identifies.
-#➡️ Bonus: Try to find what happens if you increase noise or reduce sampling rate.
+# Hints:
+# Use PolynomialLibrary(degree=2)
+# Visualize trajectories in the phase plane (x vs y)
+
+alpha = 1.0
+beta  = 0.5
+delta = 0.5
+gamma = 2.0
+
+x0 = 2.0
+y0 = 1.0
+
+def derivative(X, t, alpha, beta, delta, gamma):
+    x,y = X
+    xdot = x * (alpha - beta * y)
+    ydot = y * (-delta + gamma * x)
+    return np.array([xdot, ydot])
+
+t = np.linspace(0,30, 3001)
+
+X0 = [x0, y0]
+
+res = odeint(derivative, X0, t, args=(alpha, beta, delta, gamma))
+x, y = res.T
+
 
