f8b2254fd5ccf4d3ed24b7c5718c342cd3c808a6
Phuctran24102002 <tranchau@oregonstate.edu>
2025-10-29 12:57:26 -0700

    Update phuc_testing

--- phuc_testing/example.py
+++ phuc_testing/example.py
@@ -2,10 +2,6 @@ import numpy as np
 import pysindy as ps
 import matplotlib.pyplot as plt
 
-
-# x(t) = x0 * exp(-2*t) | x0 = 3
-# y(t) = y0 * exp(t) | y0 = 0.5
-
 t = np.linspace(0, 1, 100)
 
 x = 3 * np.exp(-2 * t)
@@ -16,4 +12,3 @@ x = np.stack((x,y), axis=-1)
 model = ps.SINDy()
 model.fit(x, t=t, feature_names=["x", "y"])
 model.print()
-
