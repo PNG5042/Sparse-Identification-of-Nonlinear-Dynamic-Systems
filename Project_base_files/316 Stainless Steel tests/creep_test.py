cb83618d42dea7a7a6348a359923d62a27de9344
thegooda <philipnguyen5050@gmail.com>
2025-11-15 15:29:32 -0800

    -Change some of the folders around

---
+++ Project_base_files/316 Stainless Steel tests/creep_test.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pysindy as ps
+from mpl_toolkits.mplot3d import Axes3D
+from pysindy.feature_library import CustomLibrary, PolynomialLibrary
+from scipy.integrate import solve_ivp
+from sklearn.metrics import r2_score
+
